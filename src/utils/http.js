009fdb448a01fded6457358bc6ea98d06c6139f4
shanwei <shanwei@yeah.net>
2021-06-22 18:20:58 +0800

    修改后端接口url

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import app from '../main'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000/api",
 });
 
 
