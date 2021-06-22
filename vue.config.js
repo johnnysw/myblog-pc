f131e6b261b882f2669ea73245d04b8a5fe1b4f5
shanwei <shanwei@yeah.net>
2021-06-22 18:34:52 +0800

    添加vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // assetsDir: './',
+  // publicPath: './',
+  outputDir: "../www",
+  lintOnSave: false,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
