0d8c045cda0d4bb293903c6bd8c52ac3074a7f24
shanwei <shanwei@yeah.net>
2021-06-23 21:40:56 +0800

    修改api地址为localhost

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import app from '../main'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://47.93.215.19:3000/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 
